3cac72a40b98ee56392ffd87f361f637fde45207
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-12-10 23:05:15 -0300

    verificando stamina

--- Pokemon test unitario/lib/pokemon.js
+++ Pokemon test unitario/lib/pokemon.js
@@ -1,8 +1,19 @@
 function pokemonBatalha(pokemon1,pokemon2) {
+
+if(pokemon1 ==="Squirtle"||pokemon2==="Squirtle"){
+    return"Squirtle";
+}
+
 if(pokemon1 ==="Squirtle"){
     return "Squirtle"
 }
 else
     return "pikachu";
 }
+
+function staminaSum(num1,num2){
+return 5;
+
+}
 module.exports.pokemonBatalha = pokemonBatalha;
+module.exports.staminaSum =staminaSum;
--- Pokemon test unitario/test/pokemon_Test.spec.js
+++ Pokemon test unitario/test/pokemon_Test.spec.js
@@ -7,9 +7,26 @@ describe("pokemon lib", () => {
         it("Squirtle ganha do charizard", () => {
             expect(Pokemon.pokemonBatalha("Squirtle", "charizard")).to.equal("Squirtle");
         });
-        it("pikachu ganha  do Evee", () =>{
-            expect(Pokemon.pokemonBatalha("pikachu", "Squirtle")).to.equal("pikachu");
+
+        it("charizard perde do Squirtle", () => {
+            expect(Pokemon.pokemonBatalha("charizard", "Squirtle")).to.equal("Squirtle");
         });
+
+        it("pikachu ganha  do butterfree ", () => {
+            expect(Pokemon.pokemonBatalha("pikachu", "butterfree ")).to.equal("pikachu");
+        });
+
+           it("bubassauro ganha de onix",() =>{
+               expect(Pokemon.pokemonBatalha("bubassauro",onix)).to.equal("bubassauro");
+           });
+
+    });
+describe("soma staminas",()=>{
+it("soma 1 com 4",()=>{
+    expect(Pokemon.staminaSum(1,4)).to.equal(5);
 });
+
+});
+
 
 });
