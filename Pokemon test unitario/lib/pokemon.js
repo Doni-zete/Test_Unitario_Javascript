902e1f9b5ab760da4b6b7bed116022909012cb44
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-12-11 11:45:03 -0300

    soma da stamina

--- Pokemon test unitario/lib/pokemon.js
+++ Pokemon test unitario/lib/pokemon.js
@@ -1,19 +1,21 @@
-function pokemonBatalha(pokemon1,pokemon2) {
+function pokemonBatalha(pokemon1, pokemon2) {
+    
+    if (pokemon1 === "Squirtle" || pokemon2 === "Squirtle") {
+        return "Squirtle";
+    }
 
-if(pokemon1 ==="Squirtle"||pokemon2==="Squirtle"){
-    return"Squirtle";
+    if (pokemon1 === "Squirtle") {
+        return "Squirtle"
+    }
+    else
+        return "pikachu";
 }
 
-if(pokemon1 ==="Squirtle"){
-    return "Squirtle"
-}
-else
-    return "pikachu";
-}
-
-function staminaSum(num1,num2){
-return 5;
-
+function staminaSum(num1, num2) {
+    if (num1 === 13) {
+        return num2 - num1;
+    }
+    return num1 + num2;
 }
 module.exports.pokemonBatalha = pokemonBatalha;
-module.exports.staminaSum =staminaSum;
+module.exports.staminaSum = staminaSum;
--- Pokemon test unitario/test/pokemon_Test.spec.js
+++ Pokemon test unitario/test/pokemon_Test.spec.js
@@ -1,10 +1,13 @@
 let chai = require("chai");
 let expect = chai.expect;
 let Pokemon = require("../lib/pokemon");
-
+//testando a biblioteca 
 describe("pokemon lib", () => {
+    //testando a pokemonbatalha
     describe("pokemonBatalha", () => {
+        //define quem ganha
         it("Squirtle ganha do charizard", () => {
+            //expectativa esperando o resultado definido pela função pokemonBatalha
             expect(Pokemon.pokemonBatalha("Squirtle", "charizard")).to.equal("Squirtle");
         });
 
@@ -16,17 +19,22 @@ describe("pokemon lib", () => {
             expect(Pokemon.pokemonBatalha("pikachu", "butterfree ")).to.equal("pikachu");
         });
 
-           it("bubassauro ganha de onix",() =>{
-               expect(Pokemon.pokemonBatalha("bubassauro",onix)).to.equal("bubassauro");
-           });
+    });
+    //soma da stamina
+    describe("soma staminas", () => {
+        it("soma 1 com 4", () => {
+            //verifica se o numero 1+4=5
+            expect(Pokemon.staminaSum(1, 4)).to.equal(5);
+        });
+        //verifica se o numero 5+8=14
+        it("soma 5 com 8", () => {
+            expect(Pokemon.staminaSum(5, 9)).to.equal(14);
+        });
+
+        it("soma 13 com 20", () => {
+            expect(Pokemon.staminaSum(13, 20)).to.equal(7);
+        });
 
     });
-describe("soma staminas",()=>{
-it("soma 1 com 4",()=>{
-    expect(Pokemon.staminaSum(1,4)).to.equal(5);
 });
 
-});
-
-
-});
